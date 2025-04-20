77fc4256cfca1449980d4b3f36734b8ed5af405b
aNimesH1728 <sahooanimesh2004@gmail.com>
2025-04-20 20:40:04 +0530

    Update LabourService.java

--- src/restaurantsystem/service/LabourService.java
+++ src/restaurantsystem/service/LabourService.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package restaurantsystem.service;
 
 import java.io.FileInputStream;
